b7b536865b0481f1a5818d1d4ee150a6c6d75309
marikodekilo <marikodekilo@gmail.com>
2023-03-02 08:50:53 +0300

    Create UdpService. Add Send and Receive methods.

--- BackgroundServiceApp/MyBackgroundService.cs
+++ BackgroundServiceApp/MyBackgroundService.cs
@@ -1,11 +1,22 @@
-﻿namespace BackgroundServiceApp
+﻿using System.Net.Sockets;
+
+namespace BackgroundServiceApp
 {
     public class MyBackgroundService : BackgroundService
     {
+        private UdpService udpService;
+
+        public MyBackgroundService()
+        {
+            this.udpService = new UdpService(new UdpClient(22220));
+        }
+
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
             while (!stoppingToken.IsCancellationRequested)
             {
+                await udpService.Send();
+                await udpService.Receive();
                 await Task.Delay(500);
             }
         }
