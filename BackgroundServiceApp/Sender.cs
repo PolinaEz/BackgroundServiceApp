e7aaba689c458700961b7e6fada76d451172367a
marikodekilo <marikodekilo@gmail.com>
2023-04-01 14:32:12 +0300

    Fix Sender.

--- BackgroundServiceApp/Sender.cs
+++ BackgroundServiceApp/Sender.cs
@@ -23,14 +23,6 @@ namespace BackgroundServiceApp
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
-            //var pointsGpx = Drivers.Gpx.OpenLayer(@"GraphHopper-Track-2023-03-16-3km.gpx");
-            //foreach (var pointGpx in pointsGpx)
-            //{
-            //    if (pointGpx.Geometry.GeometryType != GeometryType.MultiLineString) continue;
-            //    var lines = (MultiLineString)pointGpx.Geometry;
-            //    _points = ParseMultiLineString(lines.AsText());
-            //}
-
             _points = LoadPoints();
 
             using var udpClient = new UdpClient(_senderOptions.Host, _senderOptions.Port);
@@ -44,18 +36,17 @@ namespace BackgroundServiceApp
                     await Task.Delay(1000, stoppingToken);
                     Console.WriteLine($"/////////////////////////////\n {(isInvalid ? "Invalid" : "Valid")} data \n /////////////////////////////\n");
 
-                    if (_points != null)
-                        foreach (var t in _points)
-                        {
-                            t.Time = DateTime.Now;
-                            t.Sat = isInvalid ? 2 : 5;
-                            var message = t.ToString();
+                    foreach (var t in _points!)
+                    {
+                        t.Time = DateTime.Now;
+                        t.Sat = isInvalid ? 2 : 5;
+                        var message = t.ToString();
 
-                            var data = Encoding.UTF8.GetBytes(message);
-                            await udpClient.SendAsync(data, stoppingToken);
-                            _logger.LogInformation("Send: {message}", message);
-                            await Task.Delay(1000, stoppingToken);
-                        }
+                        var data = Encoding.UTF8.GetBytes(message);
+                        await udpClient.SendAsync(data, stoppingToken);
+                        _logger.LogInformation("Send: {message}", message);
+                        await Task.Delay(1000, stoppingToken);
+                    }
 
                     isInvalid = !isInvalid;
                 }
